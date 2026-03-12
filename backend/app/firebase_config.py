ef8693c1b08f6ed0e07bc87d29d068beffefa141
K.Aditya pranihith <adityapranihith123@gmail.com>
2026-03-12 23:06:58 +0530

    Update firebase_config.py

--- backend/app/firebase_config.py
+++ backend/app/firebase_config.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("app/firebase_key.json")
+firebase_creds = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+cred = credentials.Certificate(firebase_creds)
 
 firebase_admin.initialize_app(cred)
 
